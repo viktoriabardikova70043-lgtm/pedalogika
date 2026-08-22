c5e6a5cad02350ee3186ccd1d5caf82d08a1961b
viktoriabardikova70043-lgtm <viktoriabardikova70043@gmail.com>
2026-08-22 15:07:57 +0300

    Delete app/app/layout.js

--- app/app/layout.js
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: "Pedalogika" };
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="ru">
-      <body>{children}</body>
-    </html>
-  );
-}
