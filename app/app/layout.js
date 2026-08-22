118aff96eb1da400f023e1bc1cbdb8e09157e032
viktoriabardikova70043-lgtm <viktoriabardikova70043@gmail.com>
2026-08-22 14:52:13 +0300

    Create layout.js

---
+++ app/app/layout.js
@@ -0,0 +1,9 @@
+export const metadata = { title: "Pedalogika" };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body>{children}</body>
+    </html>
+  );
+}
